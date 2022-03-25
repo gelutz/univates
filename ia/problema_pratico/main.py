d06ec42081837b22418e9c0e128e7933ebde90de
gelutz <gabriel.lutz5@gmail.com>
2022-03-25 01:15:40 -0300

    update on tododolutz @aplicacoes/tododolutz

---
+++ ia/problema_pratico/main.py
@@ -0,0 +1,18 @@
+caminhao = {
+    'largura': 0,
+    'altura': 0,
+    'comprimento': 0,
+    'peso': 0
+}
+
+caminhao['altura'] = input('Altura do caminhão: ')
+caminhao['largura'] = input('Largura do caminhão: ')
+caminhao['peso'] = input('Limite de peso do caminhão: ')
+
+caixa = {
+    'largura': 0,
+    'altura': 0,
+    'comprimento': 0,
+    'peso': 0
+}
+
